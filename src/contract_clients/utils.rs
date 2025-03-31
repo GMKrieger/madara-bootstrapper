00320209d2266a062b84b837883d32093c1c48cb
Gustavo Krieger <30601749+GMKrieger@users.noreply.github.com>
2025-03-31 12:25:30 -0300

    quick fix

--- src/contract_clients/utils.rs
+++ src/contract_clients/utils.rs
@@ -145,6 +145,8 @@ pub async fn declare_contract(input: DeclarationInput<'_>) -> Felt {
             sierra_class_hash
         }
         LegacyDeclarationInputs(artifact_path, url, provider) => {
+            println!("{}", env!("CARGO_MANIFEST_DIR").to_owned());
+            println!("{}", artifact_path);
             let contract_abi_artifact: LegacyContractClass = serde_json::from_reader(
                 std::fs::File::open(env!("CARGO_MANIFEST_DIR").to_owned() + "/" + &artifact_path).unwrap(),
             )
